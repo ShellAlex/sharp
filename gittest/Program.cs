398ed71e138de429ba0de112782f25ee50752901
ShellAlex <shellalex2014@gmail.com>
2019-07-09 20:48:06 +0300

    тестовый коммит включил

--- gittest/Program.cs
+++ gittest/Program.cs
@@ -9,8 +9,21 @@ namespace gittest
         {
             Stack<string> st = new Stack<string>();
             st.Push("iniyt");
-
+            Console.WriteLine("Git измененияы");
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Git измененияы");
+            Console.WriteLine("Git измененияы");
+            Console.WriteLine("Git измененияы");
+            Console.WriteLine("Git измененияы");
+            Queue<string> q = new Queue<string>();
+
+            LinkedList<string> ll = new LinkedList<string>();
+            ll.AddFirst("loill");
+            ll.AddFirst("LiLLO");
+           // ll.AddAfter("LiLLo", "test");
+            Console.WriteLine($"ll {ll.First.Value}");
+            Console.WriteLine($"ll count:: {ll.Count}");
         }
     }
 }
